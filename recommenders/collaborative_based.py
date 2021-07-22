191808bf1c7db7c23b839aaeacf0e84e88788f57
SpontaneousHotDog89 <59890861+SpontaneousHotDog89@users.noreply.github.com>
2021-07-22 13:41:31 +0200

    Add files via upload

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -30,73 +30,13 @@
 # Script dependencies
 import pandas as pd
 import numpy as np
-import pickle
-import copy
-from surprise import Reader, Dataset
-from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
 movies_df = pd.read_csv('resources/data/movies.csv',sep = ',',delimiter=',')
 ratings_df = pd.read_csv('resources/data/ratings.csv')
 ratings_df.drop(['timestamp'], axis=1,inplace=True)
 
-# We make use of an SVD model trained on a subset of the MovieLens 10k dataset.
-model=pickle.load(open('resources/models/SVD.pkl', 'rb'))
 
-def prediction_item(item_id):
-    """Map a given favourite movie to users within the
-       MovieLens dataset with the same preference.
-
-    Parameters
-    ----------
-    item_id : int
-        A MovieLens Movie ID.
-
-    Returns
-    -------
-    list
-        User IDs of users with similar high ratings for the given movie.
-
-    """
-    # Data preprosessing
-    reader = Reader(rating_scale=(0, 5))
-    load_df = Dataset.load_from_df(ratings_df,reader)
-    a_train = load_df.build_full_trainset()
-
-    predictions = []
-    for ui in a_train.all_users():
-        predictions.append(model.predict(iid=item_id,uid=ui, verbose = False))
-    return predictions
-
-def pred_movies(movie_list):
-    """Maps the given favourite movies selected within the app to corresponding
-    users within the MovieLens dataset.
-
-    Parameters
-    ----------
-    movie_list : list
-        Three favourite movies selected by the app user.
-
-    Returns
-    -------
-    list
-        User-ID's of users with similar high ratings for each movie.
-
-    """
-    # Store the id of users
-    id_store=[]
-    # For each movie selected by a user of the app,
-    # predict a corresponding user within the dataset with the highest rating
-    for i in movie_list:
-        predictions = prediction_item(item_id = i)
-        predictions.sort(key=lambda x: x.est, reverse=True)
-        # Take the top 10 user id's from each movie with highest rankings
-        for pred in predictions[:10]:
-            id_store.append(pred.uid)
-    # Return a list of user id's
-    return id_store
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
@@ -118,31 +58,27 @@ def collab_model(movie_list,top_n=10):
 
     """
 
-    indices = pd.Series(movies_df['title'])
-    movie_ids = pred_movies(movie_list)
-    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
-    for i in movie_ids :
-        df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
-    # Getting the cosine similarity matrix
-    cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-     # Appending the names of movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
-    recommended_movies = []
-    # Choose top 50
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies_df['title'])[i])
-    return recommended_movies
+    train_sample = pd.merge(movies_df[['movieId', 'title']], ratings_df[['userId', 'movieId', 'rating']])
+    min_movie_ratings = 100
+    filter_movies = train_sample['movieId'].value_counts() > min_movie_ratings
+    filter_movies = filter_movies[filter_movies].index.tolist()
+
+    train_sample = train_sample[(train_sample['movieId'].isin(filter_movies))]
+    train_sample = train_sample[:4000000]
+    train_pivot = train_sample.pivot_table(index = ['userId'], columns = ['title'], values = 'rating')
+    
+    
+
+    ratings_1 = train_pivot[movie_list[0]]
+    ratings_2 = train_pivot[movie_list[1]]
+    ratings_3 = train_pivot[movie_list[2]]
+
+    similar_movies_1 = train_pivot.corrwith(ratings_1).dropna().sort_values(ascending = False)[1:11]
+    similar_movies_2 = train_pivot.corrwith(ratings_2).dropna().sort_values(ascending = False)[1:11]
+    similar_movies_3 = train_pivot.corrwith(ratings_3).dropna().sort_values(ascending = False)[1:11]
+
+    listings = similar_movies_1.append(similar_movies_2).append(similar_movies_3)
+    listings = listings.sort_values(ascending = False)[:top_n]
+    
+    
+    return list(listings.index)
--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -32,12 +32,12 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Importing data
-movies = pd.read_csv('resources/data/movies.csv', sep = ',',delimiter=',')
-ratings = pd.read_csv('resources/data/ratings.csv')
-movies.dropna(inplace=True)
+df_movies = pd.read_csv('resources/data/movies.csv', sep = ',',delimiter=',')
+df_imdb = pd.read_csv('resources/data/imdb_data.csv')
+df_movies.dropna(inplace=True)
 
 def data_preprocessing(subset_size):
     """Prepare data for use within Content filtering algorithm.
@@ -53,10 +53,26 @@ def data_preprocessing(subset_size):
         Subset of movies selected for content-based filtering.
 
     """
-    # Split genre data into individual words.
-    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
-    # Subset of the data
-    movies_subset = movies[:subset_size]
+
+    # Merge the movies and imdb data sets
+    # Don't remove rows containing Nan values, this will remove too much data
+    movies_content = df_movies[
+        ['movieId', 'title', 'genres']
+    ].merge(df_imdb[
+        ['movieId', 'title_cast', 'director', 'plot_keywords']
+    ], on = 'movieId', how = 'left').fillna('')
+
+    # Remove all rows contain no genres listed
+    # Data Frame indices needs to be reset to correpsond to order
+    movies_content = movies_content[movies_content['genres'] != '(no genres listed)'].reset_index()
+
+    # Concatenate genres, plot keywords, actors and director columns with a vertical line to 
+    # to maintain a consistent seperator
+    movies_content['genres_plot_cast_director'] = movies_content[
+        ['genres', 'plot_keywords', 'title_cast', 'director']
+    ].agg('|'.join, axis = 1)
+
+    movies_subset = movies_content[:subset_size]
     return movies_subset
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
@@ -82,31 +98,30 @@ def content_model(movie_list,top_n=10):
     recommended_movies = []
     data = data_preprocessing(27000)
     # Instantiating and generating the count matrix
-    count_vec = CountVectorizer()
-    count_matrix = count_vec.fit_transform(data['keyWords'])
-    indices = pd.Series(data['title'])
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+    tfidf_vec = TfidfVectorizer(analyzer = lambda x: x.split('|'))
+    tfidf_matrix = tfidf_vec.fit_transform(data['genres_plot_cast_director'])
+    indices = pd.Series(data.index, index = data['title'])
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
     # Getting the index of the movie that matches the title
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-    # Getting the indexes of the 10 most similar movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+    idx_1 = indices[movie_list[0]]
+    idx_2 = indices[movie_list[1]]
+    idx_3 = indices[movie_list[2]]
+    # Creating a list of tuples containing the movie index and similarity score
+    rank_1 = list(enumerate(cosine_sim[idx_1]))
+    rank_2 = list(enumerate(cosine_sim[idx_2]))
+    rank_3 = list(enumerate(cosine_sim[idx_3]))
+    # Sorting each list by similarity score and only returning half of the total number of recommendations
+    score_series_1 = sorted(rank_1, key = lambda x: x[1], reverse = True)[:int(top_n / 2)]
+    score_series_2 = sorted(rank_2, key = lambda x: x[1], reverse = True)[:int(top_n / 2)]
+    score_series_3 = sorted(rank_3, key = lambda x: x[1], reverse = True)[:int(top_n / 2)]
+    # Concatenate lists 
+    listings = score_series_1 + score_series_2 + score_series_3
+    # Iterate through list to make sure the chosen movies are excluded
+    listings = [x for x in listings if x[0] not in [idx_1, idx_2, idx_3]]
+    # Sorting the final recommendation list
+    listings = sorted(listings, key = lambda x: x[1], reverse = True)
 
-    # Store movie names
-    recommended_movies = []
-    # Appending the names of movies
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies['title'])[i])
+    # Get top 10 recommendations
+    movie_indices = [i[0] for i in listings[:top_n]]
+    recommended_movies = data['title'].iloc[movie_indices].values
     return recommended_movies
