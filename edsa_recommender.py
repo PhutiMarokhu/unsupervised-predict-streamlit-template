5497736275fcf0e33e88d74c2528bcd7ec77562c
SpontaneousHotDog89 <59890861+SpontaneousHotDog89@users.noreply.github.com>
2021-07-22 13:40:03 +0200

    Add files via upload

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -27,25 +27,55 @@
 """
 # Streamlit dependencies
 import streamlit as st
+st.set_page_config(
+    page_title = "AM4 Stramlit Movie Recommender",
+    page_icon="🎥"
+    )
 
 # Data handling dependencies
 import pandas as pd
 import numpy as np
+
+# Packages for visualization
+from plotly.offline import init_notebook_mode
+import plotly.graph_objects as go
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
 from recommenders.collaborative_based import collab_model
 from recommenders.content_based import content_model
 
+#Github links to data
+genome_link = "https://github.com/PhutiMarokhu/unsupervised-predict-streamlit-template/blob/a2e55a282129cf259830e907f4fb9d1f6e99b4fb/Snipped%20of%20datasets(100)/genome_scores_df.csv"
+genome_tag_link = "https://github.com/PhutiMarokhu/unsupervised-predict-streamlit-template/blob/a2e55a282129cf259830e907f4fb9d1f6e99b4fb/Snipped%20of%20datasets(100)/genome_scores_df.csv"
+imdb_link = "https://github.com/PhutiMarokhu/unsupervised-predict-streamlit-template/blob/a2e55a282129cf259830e907f4fb9d1f6e99b4fb/Snipped%20of%20datasets(100)/imdb_df.csv"
+links_link = "https://github.com/PhutiMarokhu/unsupervised-predict-streamlit-template/blob/a2e55a282129cf259830e907f4fb9d1f6e99b4fb/Snipped%20of%20datasets(100)/links_df.csv"
+movies_link = "https://github.com/PhutiMarokhu/unsupervised-predict-streamlit-template/blob/a2e55a282129cf259830e907f4fb9d1f6e99b4fb/Snipped%20of%20datasets(100)/movies_df.csv"
+tags_link = "https://github.com/PhutiMarokhu/unsupervised-predict-streamlit-template/blob/a2e55a282129cf259830e907f4fb9d1f6e99b4fb/Snipped%20of%20datasets(100)/tags_df.csv"
+test_link = "https://github.com/PhutiMarokhu/unsupervised-predict-streamlit-template/blob/a2e55a282129cf259830e907f4fb9d1f6e99b4fb/Snipped%20of%20datasets(100)/test_df.csv"
+train_link = "https://github.com/PhutiMarokhu/unsupervised-predict-streamlit-template/blob/a2e55a282129cf259830e907f4fb9d1f6e99b4fb/Snipped%20of%20datasets(100)/train_df.csv"
+
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
 
+movies_data = movies = pd.read_csv('resources/data/movies_df.csv', sep = ',',delimiter=',')
+genome_data = pd.read_csv('resources/data/genome_scores_df.csv', sep = ',',delimiter=',')
+genome_tags_data = pd.read_csv('resources/data/genome_tags_df.csv', sep = ',',delimiter=',')
+links_data = pd.read_csv('resources/data/links_df.csv', sep = ',',delimiter=',')
+#train_data = pd.read_csv('resources/data/train_df.csv', sep = ',',delimiter=',')
+test_data = pd.read_csv('resources/data/test_df.csv', sep = ',',delimiter=',')
+imdb_data = pd.read_csv('resources/data/imdb_df.csv', sep = ',',delimiter=',')
+tags_data = pd.read_csv('resources/data/tags_df.csv', sep = ',',delimiter=',')
+
+train_data_link='https://drive.google.com/file/d/11Gs_0v_JrqLay1TXisydeKhh0x5D0_wy/view?usp=sharing"'
+train_data_download='https://drive.google.com/uc?id=' + train_data_link.split('/')[-2]
+train_data = pd.read_csv(train_data_download)
 # App declaration
 def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Collected Data","Preprocessing methods", "EDA", "Model Performance", "Solution Overview"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -55,7 +85,8 @@ def main():
         # Header contents
         st.write('# Movie Recommender Engine')
         st.write('### EXPLORE Data Science Academy Unsupervised Predict')
-        st.image('resources/imgs/Image_header.png',use_column_width=True)
+        st.image('resources/imgs/Movie posters.jpeg', width =800)
+        ##st.image('resources/imgs/Image_header.png',use_column_width=True)
         # Recommender System algorithm selection
         sys = st.radio("Select an algorithm",
                        ('Content Based Filtering',
@@ -63,9 +94,9 @@ def main():
 
         # User-based preferences
         st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
-        movie_2 = st.selectbox('Second Option',title_list[25055:25255])
-        movie_3 = st.selectbox('Third Option',title_list[21100:21200])
+        movie_1 = st.selectbox('First Option',title_list[:])
+        movie_2 = st.selectbox('Second Option',title_list[:])
+        movie_3 = st.selectbox('Third Option',title_list[:])
         fav_movies = [movie_1,movie_2,movie_3]
 
         # Perform top-10 movie recommendation generation
@@ -76,6 +107,7 @@ def main():
                         top_recommendations = content_model(movie_list=fav_movies,
                                                             top_n=10)
                     st.title("We think you'll like:")
+
                     for i,j in enumerate(top_recommendations):
                         st.subheader(str(i+1)+'. '+j)
                 except:
@@ -100,6 +132,128 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    if page_selection == "Collected Data":
+        st.title("Collected data and descriptions")
+        st.subheader("We imported 8 databases for our model training and fitting. Namely:")
+
+        counter = st.slider("Select number of values to view from dataframes", 5, 20, 5)
+
+        st.write("""*1. Movies Dataset*  \n Database containing the informaion regarding the movies 
+        such as movieId, title, genres.""")
+        st.text("Observation: \n\t •Contains no null values \n\t •Has 62423 rows and 3 columns")
+        st.dataframe(movies_data.head(counter))
+        st.markdown(f"[Get movies data]({movies_link})")
+
+        st.write("""*2. IMDB Dataset*  \n Database containging info scraped from imdb like title_cast, 
+        director, runtime, budget, etc.""")
+        st.text("""Observation: \n\t •Contains null values in the title_cast, director, runtime, budget 
+        and\n\t  plot_keywords columns \n\t •Has 27278 rows and 6 columns""")
+        st.dataframe(imdb_data.head(counter))
+        st.markdown(f"[Get IMDB data]({imdb_link})")
+
+        st.write("""*3. Genome Scores Dataset*  \n Each genome tag score reflects the relevance of a tag 
+        to a movie on a scale of 0 to 1. They were computed us- ing a machine learning algorithm based 
+        on user-contributed content that includes tags, ratings and textual reviews.""")
+        st.text("""Observation: \n\t •Contains no null values \n\t •Has 15584448 rows and 3 columns \n\t 
+        •The info() method wasn't any help in displaying null values therefore we used \n\t  the isnull() 
+        and sum() methods to see if each columns contains null values""")
+        st.dataframe(genome_data.head(counter))
+        st.markdown(f"[Get genome scores data]({genome_link})")
+
+        st.write("""*4. Genome Tags Dataset*  \n  The tag genome representation is built from user input 
+        on the website, where users can apply tags to movies and rate them in order to provide the information
+         needed to make recommendations.""")
+        st.text("Observation: \n\t •Contains no null values \n\t •Has 1128 rows and 2 columns")
+        st.dataframe(genome_tags_data.head(counter))
+        st.markdown(f"[Get genome tag data]({genome_tag_link})")
+
+        st.write("""*5. Train Dataset*  \n Contains userId, movieId, ratings and timestamp of movies. These are ratings 
+        given by users for movies as well as the date and time these ratings were given.""")
+        st.text("""Observation: \n\t •Contains no null values \n\t •Has 10000038 rows and 4 columns \n\t 
+        •The info() method wasn't any help in displaying null values therefore we used \n\t  the isnull() and 
+        sum() methods to see if each columns contains null values""")
+        st.dataframe(train_data.head(counter))
+        st.markdown(f"[Get train data]({train_link})")
+        st.info(train_data.shape)
+        
+        st.write("*6. Test Dataset*  \n ")
+        st.text("""Observation: \n\t •Contains no null values \n\t •Has 5000019 rows and 2 columns \n\t 
+        •The info() method wasn't any help in displaying null values therefore we used \n\t  the isnull() 
+        and sum() methods to see if each columns contains null values""")
+        st.dataframe(test_data.head(counter))
+        st.markdown(f"[Get test data]({test_link})")
+        
+        st.write("""*7. Tags Dataset*  \n A tag is a keyword or term assigned to a piece of information. 
+        This kind of metadata helps describe an item and allows it to be found again by browsing or searching. 
+        These are added by users as extra information that may not come with the movie. """)
+        st.text("Observation: \n\t •Contains null values in the tag column \n\t •Has 1093360 rows and 4 columns")
+        st.dataframe(tags_data.head(counter))
+        st.markdown(f"[Get tags data]({tags_link})")
+
+        
+        st.write("""*8. Links Dataset*  \n Database containg the IMDB_id and TMDB_id which can be compiled to 
+        get the full link to the respective websites.""")
+        st.text("Observation: \n\t •Contains null values in the tmdbID column\n\t •Has 62423 rows and 3 columns")
+        st.dataframe(links_data.head(counter))
+        st.markdown(f"[Get links data]({links_link})")
+
+    if page_selection == "Preprocessing methods":
+        st.title("Data Preprocessing methods")
+        st.markdown('Memory reduction')
+        st.write("""Memory reduction can be performed by changing each column's daya type, 
+        to a data type that is best suited for the range of values it contains.  
+        \n**_Why is memory redcution important?_**
+        \nChoosing the right data types for your tables, stored procedures, and variables not 
+        only improves performance by ensuring a correct execution plan, but it also improves 
+        data integrity by ensuring that the correct data is stored within a database.""")
+        st.image("resources/imgs/Memory usage.png", caption = "Memory(GB) saved by choosing the correct data type",
+        use_column_width=True)
+
+
+    if page_selection == "EDA":
+        st.title("Data Exploration")
+        st.write("""There are multiple datasets containing information that we don't know if 
+        it is relevant or not. We need to explore our data to get a better understanding of it's context.""")
+        st.header("Summary of our data")
+        st.image("resources/imgs/movie database sum.png")
+        st.write("""We are working with a total of just over 62,000 movies, whose average duration is 100.3
+        minutes-roughly 1:41 minutes. Some research suggests that our attention span has been decreasing over 
+        the decades, with an average attention span of 12 seconds in 2000 to 8 seconds now. Some movie critics are 
+        wondering if movie runtimes should also decrease with the decades to keep the audience engaged. 
+        \nOur data was collected from [MovieLens.org](https://movielens.org/), a research site run 
+        by GroupLens Research at the University of Minnesota. Our database consists of 162,500 users with a combined 
+        avergae ratings of 3.53. The median of these ratings is 2.5(out of 5). So the fact that the average ratings 
+        in the database is 1 unit higher than the median tells us that on avergae, movies are well made or 
+        rather, movies are generally well received and rated by the public.""")
+
+        st.header("Movie Genres composition")
+        st.image("resources/imgs/Most Genre.png")
+        st.write("""The majority of the the genre of movies that are being watched/produced are Drama. Making up 23% 
+        of all the movies in our databases. This is possibly because drama can occur adjacent to other genres for the same movie 
+        because most movies will contain a good degree of drama to it. Be it Action, Comedy and even Animation. There are also a 
+        lot of movies which are added/published without any genre being
+         """)
+
+        st.image("resources/imgs/Ratings distri.png")
+
+        st.image("resources/imgs/Number of Ratings per year.png")
+        
+        st.image("resources/imgs/Number of ratings per user.png")
+        
+        st.image("resources/imgs/Top 10 Most Average Rated Movies.png")
+        
+        st.image("resources/imgs/Top 10 movies ratings.png")
+        
+        st.image("resources/imgs/Top 10 Most Average Rated Movies.png")
+        
+        st.markdown('Some info here soon')
+
+
+    if page_selection == "Model Performance":
+        st.title("Empty for now")
+        st.markdown('Some info here soon')
+
+
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
